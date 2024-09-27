6183f712b6d08031f40e02301bff3b615059bc68
farzonak0587 <farzonak2@Nycstudents.net>
2024-09-27 19:32:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Farzona Khabibova"; // update the part BETWEEN the "quotes"
+let userName = "farzonak0587"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
